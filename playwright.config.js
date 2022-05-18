e0957192598e7be006ba52a257836d9872955e09
KosBelozyorov <kos.belozerov@gmail.com>
2022-05-18 19:24:30 +0300

    update playwright.config and package.json

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
+// @ts-check
+const { devices } = require('@playwright/test');
 
 /**
  * Read environment variables from file.
@@ -8,10 +8,12 @@ import { devices } from '@playwright/test';
 // require('dotenv').config();
 
 /**
- * See https://playwright.dev/docs/test-configuration.
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
  */
-const config: PlaywrightTestConfig = {
-  testDir: './tests',
+const config = {
+  testDir: './tests/',
+  // testMatch: ['/tests/**/*.test.js'],
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -19,18 +21,17 @@ const config: PlaywrightTestConfig = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 5000,
   },
-  /* Run tests in files in parallel */
-  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  // repeatEach: 5,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list'], ['html', { open: 'never', outputFolder: 'my-report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -39,7 +40,9 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    // headless: false,
+    // video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -48,22 +51,24 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
+        viewport: { width: 1920, height: 900 },
       },
     },
 
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
 
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //     viewport: { width: 1920, height: 900 },
+    //   },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -104,4 +109,4 @@ const config: PlaywrightTestConfig = {
   // },
 };
 
-export default config;
+module.exports = config;
